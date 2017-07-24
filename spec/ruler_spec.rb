c2a9f66969b9ed0270a7730f9e63c84cc5c0428c
Serhii Gubenya <monoxrom@gmail.com>
2017-07-24 15:29:43 +0300

    removing old spec

--- spec/ruler_spec.rb
+++
@@ -1,9 +0,0 @@
-require '../shiny_button.rb'
-describe ShinyButton do
-  describe '#do_action'
-  it 'should change stopwatch state' do
-    subject.do_action
-    expect(subject.state).to eql('started')
-  end
-
-end
